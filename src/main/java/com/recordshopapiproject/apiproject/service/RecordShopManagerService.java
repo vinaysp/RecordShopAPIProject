26a723d2fc375d3b2f2f223ab63e77bfda6be72d
Vinay Patel <vinayspatel0@gmail.com>
2024-12-05 18:53:43 +0000

    feat: create RecordShopManagerService interface

---
+++ src/main/java/com/recordshopapiproject/apiproject/service/RecordShopManagerService.java
@@ -0,0 +1,20 @@
+package com.recordshopapiproject.apiproject.service;
+
+import com.recordshopapiproject.apiproject.model.Album;
+import com.recordshopapiproject.apiproject.model.Genre;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface RecordShopManagerService {
+
+    List<Album> getAllAlbums();
+    Album insertAlbum(Album album);
+
+    Optional<Album> getAlbumsByID(Long ID);
+    public Album updateAlbum(Long id, Album albumDetails) throws Exception;
+    public void deleteAlbum(Long ID);
+
+
+    Iterable<Album> getAlbumsByGenre(Genre genre);
+}
