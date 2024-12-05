40daa955c0325b5f27f16a07d5966fea85e805f3
Vinay Patel <vinayspatel0@gmail.com>
2024-12-05 09:42:52 +0000

    chore: initial commit

---
+++ src/test/java/com/RecordShopAPIProject/APIproject/ApIprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.RecordShopAPIProject.APIproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApIprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
