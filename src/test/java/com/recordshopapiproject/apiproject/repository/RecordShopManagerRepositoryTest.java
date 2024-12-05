8dc981f891eb07ed3ba8f16904331019da550f6d
Vinay Patel <vinayspatel0@gmail.com>
2024-12-05 17:36:10 +0000

    refactor: remove unused import

--- src/test/java/com/recordshopapiproject/apiproject/repository/RecordShopManagerRepositoryTest.java
+++ src/test/java/com/recordshopapiproject/apiproject/repository/RecordShopManagerRepositoryTest.java
@@ -7,6 +7,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
+import java.util.List;
 import static org.assertj.core.api.Assertions.assertThat;
 
 
