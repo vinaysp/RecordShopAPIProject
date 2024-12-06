583920fdd5d97e8dcf5785a6dda39679249c13ec
Vinay Patel <vinayspatel0@gmail.com>
2024-12-06 14:04:20 +0000

    refactor: change id generated value strategy to sequence

--- src/main/java/com/recordshopapiproject/apiproject/model/Album.java
+++ src/main/java/com/recordshopapiproject/apiproject/model/Album.java
@@ -11,7 +11,7 @@ import java.util.List;
 public class Album {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
     @Column(updatable = false, nullable = false)
     Long id;
 
