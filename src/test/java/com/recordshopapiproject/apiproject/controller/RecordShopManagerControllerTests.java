9a56979afafcab5789d9b3f523e898d77af065ec
Vinay Patel <vinayspatel0@gmail.com>
2024-12-09 22:54:18 +0000

    refactor:remove songs lists from testGetAllAlbumsReturnsAlbums test, add testPostMappingAddAlbum test, this class is still work in progress

--- src/test/java/com/recordshopapiproject/apiproject/controller/RecordShopManagerControllerTests.java
+++ src/test/java/com/recordshopapiproject/apiproject/controller/RecordShopManagerControllerTests.java
@@ -10,17 +10,20 @@ import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 import java.util.ArrayList;
 import java.util.List;
 
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 @AutoConfigureMockMvc
 @SpringBootTest
@@ -49,14 +52,10 @@ class RecordShopManagerControllerTests {
         List<Album> albums = new ArrayList<>();
         Artist me = new Artist(1L,"me");
         Artist m3 = new Artist(2L,"m3");
-        List<String> songs2 = List.of("test 1", "test 2", "test 3", "test 4");
-        List<String> songs1 = List.of("test 1", "test 2", "test 3");
-        List<String> songs3 = List.of("test 1", "test 2");
-        List<String> songs4 = List.of("test 1");
-        albums.add(new Album(1L,"Do your best", me, 2022, Genre.Rock, songs1, "motivational", 1, 9999999.99));
-        albums.add(new Album(2L,"keep going", me, 2024, Genre.Pop, songs2, "very motivational", 12, 10.50));
-        albums.add(new Album(3L,"Try it, fix it", me, 2021, Genre.Lofi, songs3, "motivationalllll", 1, 999.99));
-        albums.add(new Album(4L,"Do your best", m3, 2022, Genre.Rock, songs1, "motivational", 1, 9999999.99));
+        albums.add(new Album(1L,"Do your best", me, 2022, Genre.Rock, "motivational", 1, 9999999.99));
+        albums.add(new Album(2L,"keep going", me, 2024, Genre.Pop,"very motivational", 12, 10.50));
+        albums.add(new Album(3L,"Try it, fix it", me, 2021, Genre.Lofi, "motivationalllll", 1, 999.99));
+        albums.add(new Album(4L,"Do your best", m3, 2022, Genre.Rock,"motivational", 1, 9999999.99));
 
         when(mockRecordShopManagerImpl.getAllAlbums()).thenReturn(albums);
 
@@ -74,5 +73,26 @@ class RecordShopManagerControllerTests {
 
     }
 
+    @Test
+    public void testPostMappingAddAlbum() throws Exception{
+        Artist me = new Artist(1L,"me");
+        List<String> songs2 = List.of("test 1", "test 2", "test 3", "test 4");
+        Album album = new Album(3L,"you got this",me,2090,Genre.Lofi,"slowly getting there", 1, 9.99);
+
+        when(mockRecordShopManagerImpl.insertAlbum(any(Album.class))).thenReturn(album);
+
+        this.mockMvcController.perform(
+                        MockMvcRequestBuilders.post("/api/v1/recordShop")
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .content(mapper.writeValueAsString(album)))
+                                .andExpect(MockMvcResultMatchers.status().isCreated())
+                                .andExpect(MockMvcResultMatchers.jsonPath("$.id").value(3L))
+                                .andExpect(MockMvcResultMatchers.jsonPath("$.name").value("you got this"))
+                                        .andDo(MockMvcResultHandlers.print());
+
+
+        verify(mockRecordShopManagerImpl, times(1)).insertAlbum(any(Album.class));
+
+    }
 
 }
