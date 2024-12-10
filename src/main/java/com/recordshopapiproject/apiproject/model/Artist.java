8a0dc317f62f2e004ba59859dc78cb3ee4fff124
Vinay Patel <vinayspatel0@gmail.com>
2024-12-10 22:32:22 +0000

    refactor: add name to column name to id property, make artistName nullable false,change albums property to set and annotate with ManytoMany annotation and Jointable annotation, add addAlbum and add removeAlbum helper methods

--- src/main/java/com/recordshopapiproject/apiproject/model/Artist.java
+++ src/main/java/com/recordshopapiproject/apiproject/model/Artist.java
@@ -5,8 +5,9 @@ import jakarta.persistence.*;
 import lombok.*;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
-
+import java.util.Set;
 
 @Setter
 @Getter
@@ -15,14 +16,21 @@ public class Artist{
 
     @Id
     //@GeneratedValue(strategy = GenerationType.AUTO)
-    @Column
+    @Column(name = "artist_id")
     Long id;
 
-    @Column
+    @Column(nullable = false)
     String artistName;
 
-    @OneToMany(mappedBy = "artist", cascade = CascadeType.ALL)
-    final List<Album> albums = new ArrayList<>();
+//    @OneToMany(mappedBy = "artist", cascade = CascadeType.ALL)
+//    final List<Album> albums = new ArrayList<>();
+    @ManyToMany(cascade = {CascadeType.PERSIST, CascadeType.MERGE})
+    @JoinTable(
+        name = "artist_album",
+        joinColumns = @JoinColumn(name = "artist_id"),
+        inverseJoinColumns = @JoinColumn(name = "album_id")
+    )
+    private Set<Album> albums = new HashSet<>();
 
     public Artist(Long id,String artistName) {
         this.id = id;
@@ -35,4 +43,13 @@ public class Artist{
 
     Artist(){};
 
+    public void addAlbum(Album album) {
+        this.albums.add(album);
+        album.getArtists().add(this);
+    }
+
+    public void removeAlbum(Album album) {
+        this.albums.remove(album);
+        album.getArtists().remove(this);
+    }
 }
