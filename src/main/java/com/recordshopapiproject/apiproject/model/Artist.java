108e79f4e4df66f2cf48919e2012d872567a23f1
Vinay Patel <vinayspatel0@gmail.com>
2024-12-13 12:26:46 +0000

    refactor: import Array function, remove column name for id property, change relationship mapping for albums property to One to Many with @JsonBackReference, comment out helper methods

--- src/main/java/com/recordshopapiproject/apiproject/model/Artist.java
+++ src/main/java/com/recordshopapiproject/apiproject/model/Artist.java
@@ -1,9 +1,11 @@
 package com.recordshopapiproject.apiproject.model;
 
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
 import jakarta.persistence.*;
 import lombok.*;
 
+import java.sql.Array;
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
@@ -15,8 +17,8 @@ import java.util.Set;
 public class Artist{
 
     @Id
-    //@GeneratedValue(strategy = GenerationType.AUTO)
-    @Column(name = "artist_id")
+//    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column
     Long id;
 
     @Column(nullable = false)
@@ -24,13 +26,15 @@ public class Artist{
 
 //    @OneToMany(mappedBy = "artist", cascade = CascadeType.ALL)
 //    final List<Album> albums = new ArrayList<>();
-    @ManyToMany(cascade = CascadeType.ALL)
-    @JoinTable(
-        name = "artist_album",
-        joinColumns = @JoinColumn(name = "artist_id"),
-        inverseJoinColumns = @JoinColumn(name = "album_id")
-    )
-    private Set<Album> albums = new HashSet<>();
+//    @ManyToMany(cascade = CascadeType.ALL)
+//    @JoinTable(
+//        name = "artist_album",
+//        joinColumns = @JoinColumn(name = "artist_id"),
+//        inverseJoinColumns = @JoinColumn(name = "album_id")
+//    )
+    @JsonBackReference
+    @OneToMany(mappedBy = "artist")
+    private List<Album> albums = new ArrayList<>();
 
     public Artist(Long id,String artistName) {
         this.id = id;
@@ -43,13 +47,13 @@ public class Artist{
 
     Artist(){};
 
-    public void addAlbum(Album album) {
-        this.albums.add(album);
-        album.getArtists().add(this);
-    }
-
-    public void removeAlbum(Album album) {
-        this.albums.remove(album);
-        album.getArtists().remove(this);
-    }
+//    public void addAlbum(Album album) {
+//        this.albums.add(album);
+//        album.getArtist().add(this);
+//    }
+//
+//    public void removeAlbum(Album album) {
+//        this.albums.remove(album);
+//        album.getArtist().remove(this);
+//    }
 }
