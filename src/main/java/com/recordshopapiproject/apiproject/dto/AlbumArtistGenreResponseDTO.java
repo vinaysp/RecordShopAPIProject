a3b219220e3f3bb45edae5ebd3146e2812205442
Vinay Patel <vinayspatel0@gmail.com>
2025-01-28 15:06:10 +0000

    feat: create AlbumArtistGenreResponseDTO

---
+++ src/main/java/com/recordshopapiproject/apiproject/dto/AlbumArtistGenreResponseDTO.java
@@ -0,0 +1,29 @@
+package com.recordshopapiproject.apiproject.dto;
+
+import com.recordshopapiproject.apiproject.model.Album;
+import lombok.Data;
+
+@Data
+public class AlbumArtistGenreResponseDTO {
+    private Long albumId;
+    private Long artistId;
+    private String artistName;
+    private String albumName;
+    private String albumGenre;
+    private int albumReleaseYear;
+    private int stock;
+    private double price;
+
+    public AlbumArtistGenreResponseDTO(Album album){
+        this.albumId = album.getId();
+        this.artistId = album.getArtist() != null ? album.getArtist().getId() : null;
+        this.artistName = album.getArtist() != null ? album.getArtist().getArtistName() : null;
+        this.albumName = album.getName();
+        this.albumGenre = album.getGenre().name();
+        this.albumReleaseYear = album.getReleaseYear();
+        this.stock = album.getStock();
+        this.price = album.getPrice();
+    }
+
+
+}
