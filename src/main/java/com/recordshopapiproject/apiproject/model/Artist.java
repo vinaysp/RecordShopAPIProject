eb6971d6e829a6f021686cb34d859f25da4c8bb8
Vinay Patel <vinayspatel0@gmail.com>
2025-01-28 20:29:59 +0000

    refactor: change Generation Type in Generated Value annotation for Artist id to IDENTITY from AUTO

--- src/main/java/com/recordshopapiproject/apiproject/model/Artist.java
+++ src/main/java/com/recordshopapiproject/apiproject/model/Artist.java
@@ -17,7 +17,7 @@ import java.util.Set;
 public class Artist{
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column
     Long id;
 
