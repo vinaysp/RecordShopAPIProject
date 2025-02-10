c2c94b16b54baadadee26f32003a8ade69241584
Vinay Patel <vinayspatel0@gmail.com>
2025-02-10 16:08:00 +0000

    remove redundant abstract methods and unused imports

--- src/main/java/com/recordshopapiproject/apiproject/service/RecordShopManagerService.java
+++ src/main/java/com/recordshopapiproject/apiproject/service/RecordShopManagerService.java
@@ -1,22 +1,13 @@
 package com.recordshopapiproject.apiproject.service;
 
 import com.recordshopapiproject.apiproject.dto.AlbumArtistGenreResponseDTO;
-import com.recordshopapiproject.apiproject.model.Album;
-
-
 import java.util.List;
-import java.util.Optional;
 
 public interface RecordShopManagerService {
 
-//    List<Album> getAllAlbums();
     List<AlbumArtistGenreResponseDTO> getResponseDTO();
-//    Album insertAlbum(Album album);
     AlbumArtistGenreResponseDTO getAlbumByIdReturnDTO(Long ID) throws Exception;
-//    Optional<Album> getAlbumsByID(Long ID);
-//    public Album updateAlbum(Long id, Album albumDetails) throws Exception;
     AlbumArtistGenreResponseDTO updateAlbumUsingDTO(Long id, AlbumArtistGenreResponseDTO albumDTODetails) throws Exception;
-    public void deleteAlbum(Long ID);
-//    Album getAlbumById(Long ID) throws Exception;
+    void deleteAlbum(Long ID);
     AlbumArtistGenreResponseDTO insertAlbumFromDTO(AlbumArtistGenreResponseDTO albumArtistGenreResponseDTO);
 }
