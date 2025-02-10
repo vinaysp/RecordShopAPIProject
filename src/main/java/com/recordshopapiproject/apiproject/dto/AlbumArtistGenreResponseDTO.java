4f2d547d73815e0020a171c73632bc2a183300d4
Vinay Patel <vinayspatel0@gmail.com>
2025-02-10 15:18:43 +0000

    refactor: remove redundant code from class constructor

--- src/main/java/com/recordshopapiproject/apiproject/dto/AlbumArtistGenreResponseDTO.java
+++ src/main/java/com/recordshopapiproject/apiproject/dto/AlbumArtistGenreResponseDTO.java
@@ -17,11 +17,6 @@ public class AlbumArtistGenreResponseDTO {
     private String albumImageUrl;
 
     public AlbumArtistGenreResponseDTO(Album album){
-
-        if(album == null){
-            throw new IllegalArgumentException("Album cannot be null");
-        }
-
         this.albumId = album.getId();
         this.artistId = album.getArtist() != null ? album.getArtist().getId() : null;
         this.artistName = album.getArtist() != null ? album.getArtist().getArtistName() : null;
