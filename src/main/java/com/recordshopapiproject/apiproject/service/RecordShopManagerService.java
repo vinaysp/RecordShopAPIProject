0a60d079ef4a61a148c80ac5617ebb458b8eb0b4
Vinay Patel <vinayspatel0@gmail.com>
2025-02-10 15:40:57 +0000

    refactor: comment out redundant abstract methods

--- src/main/java/com/recordshopapiproject/apiproject/service/RecordShopManagerService.java
+++ src/main/java/com/recordshopapiproject/apiproject/service/RecordShopManagerService.java
@@ -9,14 +9,14 @@ import java.util.Optional;
 
 public interface RecordShopManagerService {
 
-    List<Album> getAllAlbums();
+//    List<Album> getAllAlbums();
     List<AlbumArtistGenreResponseDTO> getResponseDTO();
-    Album insertAlbum(Album album);
+//    Album insertAlbum(Album album);
     AlbumArtistGenreResponseDTO getAlbumByIdReturnDTO(Long ID) throws Exception;
 //    Optional<Album> getAlbumsByID(Long ID);
-    public Album updateAlbum(Long id, Album albumDetails) throws Exception;
+//    public Album updateAlbum(Long id, Album albumDetails) throws Exception;
     AlbumArtistGenreResponseDTO updateAlbumUsingDTO(Long id, AlbumArtistGenreResponseDTO albumDTODetails) throws Exception;
     public void deleteAlbum(Long ID);
-    Album getAlbumById(Long ID) throws Exception;
+//    Album getAlbumById(Long ID) throws Exception;
     AlbumArtistGenreResponseDTO insertAlbumFromDTO(AlbumArtistGenreResponseDTO albumArtistGenreResponseDTO);
 }
